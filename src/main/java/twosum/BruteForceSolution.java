536ba23a4a21172765ba1837ba8441bdbbc7a838
ivantruong <kareshi@gmail.com>
2018-05-23 22:27:10 +0100

    Added two sum HashMap solution

--- src/main/java/twosum/BruteForceSolution.java
+++ src/main/java/twosum/BruteForceSolution.java
@@ -1,6 +1,6 @@
 package twosum;
 
-public class Solution {
+public class BruteForceSolution {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0; i<nums.length; i++) {
             for(int j=i+1; j<nums.length;j++) {
---
+++ src/main/java/twosum/HashMapSolution.java
@@ -0,0 +1,24 @@
+package twosum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class HashMapSolution {
+    public int[] twoSum(int[] nums, int target) {
+        if(nums == null || nums.length < 2) {
+            throw new IllegalArgumentException("nums must have at least 2 elements");
+        }
+
+        Map<Integer,Integer> valuePositions = new HashMap<>();
+        for(int i=0; i<nums.length; i++) {
+            int lookup = target - nums[i];
+            Integer position = valuePositions.get(lookup);
+            if(position != null) {
+                return new int[]{position, i};
+            } else {
+                valuePositions.put(nums[i], i);
+            }
+        }
+        return new int[0];
+    }
+}
