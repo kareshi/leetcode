ae5bdea8b6b895e448a394715e8370bb1cf69190
ivantruong <kareshi@gmail.com>
2018-05-24 22:24:42 +0100

    Add rotate array solution

---
+++ src/main/java/rotatearray/Solution.java
@@ -0,0 +1,14 @@
+package rotatearray;
+
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        int length = nums.length;
+        int[] temp = new int[length];
+        for(int i=0; i<length; i++) {
+            temp[(i+k)%length] = nums[i];
+        }
+        for(int i=0; i<length; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
