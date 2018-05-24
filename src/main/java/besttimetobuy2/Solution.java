14706e43956f362d6312ae84c801a9f6b07174d6
ivantruong <kareshi@gmail.com>
2018-05-24 22:18:59 +0100

    Upgraded best time to buy 2 solution

--- src/main/java/besttimetobuy2/Solution.java
+++ src/main/java/besttimetobuy2/Solution.java
@@ -3,21 +3,10 @@ package besttimetobuy2;
 public class Solution {
     public int maxProfit(int[] prices) {
         int profit = 0;
-        int current = -1;
-        for(int i=0; i<prices.length - 1; i++) {
-            if(current == -1) {
-                if(prices[i+1] > prices[i]) { //time to buy !
-                    current = prices[i];
-                }
-            } else {
-                if( prices[i] > prices[i+1] ) {//time to sell !
-                    profit += prices[i] - current;
-                    current = -1;
-                }
+        for(int i=1; i<prices.length ; i++) {
+            if(prices[i] > prices[i-1]) {
+                profit += prices[i]-prices[i-1];
             }
-        }
-        if(current != -1) {
-            profit += prices[prices.length - 1] - current;
         }
         return profit;
     }
