dc3411c509296596f0acad09da1ebc6112a9f4b6
ivantruong <kareshi@gmail.com>
2018-05-25 21:56:10 +0100

    Add Intersection of two arrays

---
+++ src/main/java/easy/array/intersectionoftwoarrays/Solution.java
@@ -0,0 +1,27 @@
+package easy.array.intersectionoftwoarrays;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int[] res = new int[Math.min(nums1.length, nums2.length)];
+        while(i<nums1.length && j<nums2.length) {
+            if(nums1[i] < nums2[j]) {
+                i++;
+            } else if(nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                res[k] = nums1[i];
+                i++;
+                j++;
+                k++;
+            }
+        }
+        return Arrays.copyOfRange(res, 0, k);
+    }
+}
