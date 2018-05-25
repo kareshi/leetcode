52184cee80c192a5ca5b1d6cfc8b2ed38ea44c56
ivantruong <kareshi@gmail.com>
2018-05-25 21:32:26 +0100

    Add remove duplicates solution

---
+++ src/main/java/removedeplicates/Solution.java
@@ -0,0 +1,14 @@
+package removedeplicates;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int slow = 0;
+        for(int i= 1; i<nums.length; i++) {
+            if(nums[i] > nums[slow]) {
+                slow++;
+                nums[slow] = nums[i];
+            }
+        }
+        return slow+1;
+    }
+}
