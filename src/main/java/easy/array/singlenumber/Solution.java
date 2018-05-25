4f8a1789c9ab89bb164c0f8997434ae93021aacf
ivantruong <kareshi@gmail.com>
2018-05-25 22:07:15 +0100

    Improve Single Number

--- src/main/java/easy/array/singlenumber/Solution.java
+++ src/main/java/easy/array/singlenumber/Solution.java
@@ -3,15 +3,11 @@ package easy.array.singlenumber;
 import java.util.Arrays;
 
 public class Solution {
-
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0; i<nums.length - 1; i=i+2) {
-            if(nums[i] != nums[i+1]) {
-                return nums[i];
-            }
+        int result = 0;
+        for(int i=0; i<nums.length; i++) {
+            result ^= nums[i];
         }
-        return nums[nums.length -1];
+        return result;
     }
-
 }
